5a3aeba0679e62ffac6317ae66fab137dd6ba63c
Admin <kira1023no@gmail.com>
2026-07-21 01:19:13 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,1 @@
-BOT_TOKEN = "8924404593:AAF76CSn_TWAk84MsTZVx1f4z741IgZs8pM"
 
-# Базовые настройки
-MAX_SPEND_RUB_PER_CHECK = 200   # Максимум 200 рублей (2000 баллов) за чек
-BONUS_EXPIRE_DAYS = 180        # Срок жизни баллов
-REMIND_DAYS_BEFORE = 3         # За сколько дней напоминать
-
-# Для парсинга цен (по умолчанию Москва)
-DEFAULT_CITY = "Москва"
