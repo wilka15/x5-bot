caaa4d300bbf6015cad1ec45b6dea67f089baa25
Admin <kira1023no@gmail.com>
2026-07-21 11:50:03 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
     raise ValueError("❌ BOT_TOKEN не найден! Установите переменную окружения BOT_TOKEN")
 
-MAX_SPEND_RUB_PER_CHECK = 20
+MAX_SPEND_RUB_PER_CHECK = 200
 BONUS_EXPIRE_DAYS = 180
 REMIND_DAYS_BEFORE = 3
 DEFAULT_CITY = "Москва"
